c0b7d2e76181d4d99840ccbbd8fd91bb26884f3b
Thabo <codebahale@gmail.com>
2020-11-23 08:59:20 +0200

    retry

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\teddy bear\documents\programming world\django\classifier\backend\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
